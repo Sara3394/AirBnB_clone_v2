754c744071635058586baf4fc43b3ab0180d2261
Sara3394 <hailu.serafina@yahoo.com>
2023-01-26 18:49:59 +0300

    All

---
+++ models/user.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import hashlib
+
+
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        h = hashlib.new('md5')
+        h.update(b"password")
+        email = Column(String(128), nullable=False)
+        _password = Column("password", String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        """getter for password"""
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        """setter for password"""
+        self._password = hashlib.md5(value.encode()).hexdigest()
